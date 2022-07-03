2396e1d88bdaad95d37c24672b505425f5e165cc
Filipe Moura <filipeasm18@gmail.com>
2022-07-03 18:11:58 -0300

    feat: future netuno bot impl

---
+++ Netuninho.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands as c
+
+import netuno
+
+intents = discord.Intents.all()
+intents.members = True
+intents.typing = False
+
+bot = c.Bot(intents = intents, command_prefix="-", help_command=None)
+
+@bot.event
+async def on_ready():
+  print(f"{bot.user.name} running!")
+  # loading commands
+  netuno.events.__init__(bot)
