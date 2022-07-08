e51c52353d095eb624c0c1ccd1c76673ecb49745
Filipe Moura <filipeasm18@gmail.com>
2022-07-08 19:40:40 -0300

    fix: small errors fixes

--- netuno/errors/standard.py
+++ netuno/errors/standard.py
@@ -2,8 +2,9 @@ from discord.ext import commands as cmd
 from netuno.messages import permission_denied
 
 
-async def handle(ctx, error):
+async def handle_chat(ctx, error):
   if isinstance(error, cmd.BadArgument):
     await ctx.send("Specify a **valid** user!")
   elif isinstance(error, cmd.MissingPermissions):
     await ctx.send(embed=permission_denied())
+
